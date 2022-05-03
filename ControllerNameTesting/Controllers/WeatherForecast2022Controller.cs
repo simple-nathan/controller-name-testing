eb3d7cbdc6492fcd565ca2570ebfac01b7ef2252
Nathan Waterman <nwaterman@ntst.com>
2022-05-03 17:45:15 -0500

    Initial code push

---
+++ ControllerNameTesting/Controllers/WeatherForecast2022Controller.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerNameTesting.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecast2022Controller : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecast2022Controller> _logger;
+
+    public WeatherForecast2022Controller(ILogger<WeatherForecast2022Controller> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+}
